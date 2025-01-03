95f18fe79431a69962e334fbbc7ca7afdc013350
ninayao <ninayao@potix.com>
2025-01-03 16:30:45 +0800

    feat: create initial UI for login page using ZK MVC

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,11 +2,22 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RequestMapping( "spring-boot")
+@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    @GetMapping("/hello")
+    public String firstApp(@RequestParam(value = "name", defaultValue = "Coder") String name) {
+        return String.format("Congratulations  %s, you've built your first Spring Boot Application from Scratch!!", name);
+    }
 }
+
 
